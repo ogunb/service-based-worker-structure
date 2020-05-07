8fde29276aab108dd988dfee012ff8b609a772db
Ogün Babacan <ogun.babacan@trendyol.com>
2020-05-07 17:42:10 +0300

    fix

--- main.worker.js
+++ main.worker.js
@@ -20,5 +20,5 @@ self.addEventListener('message', async ({ data: { eventName, data } }) => {
         };
     }
 
-    postMessage({ value: returnValue, hasError: false });
+    postMessage(returnValue);
 });
